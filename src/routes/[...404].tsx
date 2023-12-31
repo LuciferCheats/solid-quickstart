1c1a35f53d9d91b29bacb72e2fe83885ff903f2d
LuciferCheats <151157041+LuciferCheats@users.noreply.github.com>
2023-12-31 08:13:32 +0530

    Update [...404].tsx

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -4,15 +4,15 @@ import { HttpStatusCode } from "solid-start/server";
 export default function NotFound() {
   return (
     <main>
-      <Title>Not Found</Title>
+      <Title>Madarchod Saale</Title>
       <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
+      <h1>Madarchod Saale</h1>
       <p>
         Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
+        <a href="https://telegram.me/Lucifer_Owner_Exe" target="_blank">
+          https://telegram.me/Lucifer_Owner_Exe
         </a>{" "}
-        to learn how to build SolidStart apps.
+        Papa Hai Beta
       </p>
     </main>
   );

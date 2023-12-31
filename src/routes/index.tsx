5c46220883f5f4713dce8dcc9fd754e49dc701ac
LuciferCheats <151157041+LuciferCheats@users.noreply.github.com>
2023-12-31 07:43:57 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,15 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>DDOS TESTING BY LUCIFER</Title>
+      <h1>DDOS TESTING BY LUCIFER!</h1>
       <Counter />
       <p>
         Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
+        <a href="https://telegram.me/Lucifer_Owner_Exe" target="_blank">
+          https://telegram.me/Lucifer_Owner_Exe
         </a>{" "}
-        to learn how to build SolidStart apps.
+        PROTECTOR OF THIS SITE FUCK OFF DDOS TOOLS
       </p>
     </main>
   );
